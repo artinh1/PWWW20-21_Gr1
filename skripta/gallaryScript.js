bfcaf8281567893fb3398db2f25e8b2640df8f72
endritbeqa18 <45493292+endritbeqa18@users.noreply.github.com>
2020-11-30 13:13:15 +0100

    gallary bg-modal

--- skripta/gallaryScript.js
+++ skripta/gallaryScript.js
@@ -2,8 +2,9 @@ var MLBsection;
 var UFCsection;
 var ShowMoreButton;
 var ShowLessButton;
-
-
+var BgModal;
+var CloseButton;
+var subscribeButton;
 
 
 function start()
@@ -12,19 +13,35 @@ function start()
      UFCsection=document.getElementById("UFCSection");
      ShowMoreButton=document.getElementById("ShowMore");
      ShowLessButton=document.getElementById("ShowLess");
+     BgModal=document.getElementById("BgModal");
+     CloseButton=document.getElementById("CloseButton");
+     subscribeButton=document.getElementById("subscribeButton");
     
+    
+    CloseButton.addEventListener("click",closeBgModal,false);
+    subscribeButton.addEventListener("click",closeBgModal,false);
     ShowMoreButton.addEventListener("click",showMore,false);
     ShowLessButton.addEventListener("click",showLess,false);
-    console.log("jesus");
-    console.log(MLBsection.style["display"]);
-
+        
 }
 
 window.addEventListener("load",start,false);
 
 
+
+
+
+
+function closeBgModal()
+{
+    BgModal.style["display"]="none";
+}
+
+
+
+
 function showMore()
-{console.log("jesusmore");
+{
     if(MLBsection.style["display"]==="")
         {
             MLBsection.style["display"]="block";
@@ -42,7 +59,7 @@ function showMore()
 
 
 function showLess()
-{console.log("jesusless");
+{
     if(UFCsection.style["display"]==="block")
         {console.log("got it");
             UFCsection.style["display"]="";
