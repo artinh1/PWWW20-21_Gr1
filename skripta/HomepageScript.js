b938a31543a48e2ca009ad2fe2ba296d55c593bc
endritbeqa18 <45493292+endritbeqa18@users.noreply.github.com>
2021-01-02 19:36:11 +0100

    homepage form validation

--- skripta/HomepageScript.js
+++ skripta/HomepageScript.js
@@ -6,6 +6,8 @@ var ModalSignUpButton;
 var ModalLogInButton;
 var SignUpModal;
 var LogInModal;
+var SignUpFormValues=[];
+var LoginFormValues=[];
 
 
 
@@ -20,6 +22,18 @@ function start()
     ModalLogInButton=document.getElementById("modalLogInButton");
     SignUpModal=document.getElementById("SignUpModal");
     LogInModal=document.getElementById("LogInModal");
+    
+    SignUpFormValues.push(document.getElementById("SignUpName"));
+    SignUpFormValues.push(document.getElementById("SignUpSurname"));
+    SignUpFormValues.push(document.getElementById("SignUpEmail"));
+    SignUpFormValues.push(document.getElementById("SignUpPassword"));
+    
+    LoginFormValues.push(document.getElementById("LogInEmail"));
+    LoginFormValues.push(document.getElementById("LogInPassword"));
+    
+    
+    
+    
     
     LogInCloseButton.addEventListener("click",closeBgModal,false);
     SignUpCloseButton.addEventListener("click",closeBgModal,false);
@@ -37,11 +51,25 @@ window.addEventListener("load",start,false);
 
 
 function closeBgModal()
-{
+{ 
+    if(event.target.id===("modalSignUpButton"))
+        {
+            event.preventDefault();
+         if(validateForm(SignUpFormValues)){}
+            else{return false;}
+        }
+    if(event.target.id===("modalLogInButton"))
+        {
+            event.preventDefault();
+         if(validateForm(LoginFormValues)){}
+            else{return false;}
+        }
     LogInModal.style["display"]="none";
     SignUpModal.style["display"]="none";	
 	event.preventDefault();
     document.getElementsByTagName('body')[0].style.overflow='auto';
+    clearForm(SignUpFormValues);
+    clearForm(LoginFormValues);
      
 }
 
@@ -59,5 +87,29 @@ function openBgModal()
     document.getElementsByTagName('body')[0].style.overflow='hidden';
     }
 }
+
+
+
+
+function validateForm(formValues)
+{
+    for(var i=0; i<formValues.length; i++)
+        {
+            if(formValues[i].value===""){window.alert("The input for "+formValues[i].getAttribute("placeholder")+" field is missing");
+                                          return false;}
+        }
+   return true;
+}
+
+function clearForm(formValues)
+{
+    for(var i=0; i<formValues.length; i++)
+        {
+            formValues[i].value="";
+        }
+}
+
+
+
 
 
